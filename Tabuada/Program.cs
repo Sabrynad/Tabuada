799667bb15bf4a176a02f80b2f757f51801aae47
Sabryna <sabrynadeangelo@gmail.com>
2022-07-19 15:14:42 -0300

    Atividade de Tbuada da lista 4

---
+++ Tabuada/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int resultado, contador, numero;
+
+            Console.WriteLine("Mallu, digite um numero para criar uma tabuada: ");
+
+
+            numero = Int32.Parse(Console.ReadLine());
+
+            for (contador = 0; contador <= 10; ++contador)
+            {
+
+                resultado = numero * contador;
+
+                Console.WriteLine(numero + " X " + contador + " = " + resultado);
+            }
+             Console.ReadLine();
+
+        }
+    }
+}
+
+
